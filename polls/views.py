2dad5d07f753bbf3c32f8cf86827750a24f81a71
LorenzoTurrino <lorenzo.turrino@gmail.com>
2016-06-19 21:38:39 +0100

    add template rendering

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from django.template import loader
+
 from .models import Question
 
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_questions])
-    return HttpResponse(output)
+    pars = {
+        'latest_questions': latest_questions,
+    }
+    return render(request, 'polls/index.html', pars)
+
+    # template = loader.get_template('polls/index.html')
+    # output = template.render(pars, request)
+    # return HttpResponse(output)
 
 def detail(request, question_id):
     response = "You're looking at question %s."
