5b86a17f95f3e0200204a83446e1e96a1a7b30f0
LorenzoTurrino <lorenzo.turrino@gmail.com>
2016-06-19 19:27:07 +0100

    change question import to alternative method

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Question
 
-admin.site.register(Question)
+# from .models import Question
+# admin.site.register(Question)
+
+from . import models
+
+admin.site.register(models.Question)
